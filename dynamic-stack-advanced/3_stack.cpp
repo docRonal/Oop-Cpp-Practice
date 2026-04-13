2ca77312a72764f81cb62025dd91772e39543db2
docRonal <formyselfandother1@gmail.com>
2026-04-13 16:10:46 +0200

    Implement main function for CStack operations

--- dynamic-stack-advanced/3_stack.cpp
+++ dynamic-stack-advanced/3_stack.cpp
@@ -1,1 +1,85 @@
+#include <string.h>
+#include <iostream>
 
+using namespace std;
+#include "3_stack.h"
+
+int main()
+{
+    int a, b;
+    CStack stack1;
+
+    stack1.push(1);
+    stack1.push(2);
+
+    a = stack1.pop();
+    b = stack1.pop();
+
+    cout << a << ' ' << b << endl; // Prints: "2 1"
+    if (a != 2 || b != 1)
+    {
+        cerr << "Result 1 is wrong" << endl;
+        return -1;
+    }
+
+// Do tego miejsca: na 60%
+#if defined(FOR_80) || defined(FOR_100)
+
+    stack1.push(1);
+    stack1.push(2);
+
+    cout << stack1 << endl; // Prints: "2 1"
+
+    CStack stack2(stack1);
+
+    a = stack1.pop();
+    b = stack2.pop();
+
+    cout << a << ' ' << b << endl; // Prints: "2 2"
+    if (a != 2 || b != 2)
+    {
+        cerr << "Result 2 is wrong" << endl;
+        return -1;
+    }
+
+    stack2.push(3);
+
+    a = stack1.pop();
+    b = stack2.pop();
+
+    cout << a << ' ' << b << endl; // Prints: "1 3"
+    if (a != 1 || b != 3)
+    {
+        cerr << "Result 3 is wrong" << endl;
+        return -1;
+    }
+
+// Do tego miejsca: na 80%
+#if defined(FOR_100)
+
+    stack1.push(1);
+    stack1.push(2);
+
+    stack2.push(3);
+    stack2.push(4);
+
+    CStack stack3;
+    (stack3 = stack1) = stack2;
+
+    a = stack1.pop();
+    b = stack2.pop();
+    int c = stack3.pop();
+
+    cout << a << ' ' << b << ' ' << c << endl; // Prints: "2 4 4"
+    if (a != 2 || b != 4 || c != 4)
+    {
+        cerr << "Result 2 is wrong" << endl;
+        return -1;
+    }
+
+// Do tego miejsca: na 100%
+#endif /*defined(FOR_100)*/
+#endif /*defined(FOR_80) || defined(FOR_100)*/
+
+    return 0;
+}
